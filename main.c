dac89e04e0bd890672cd1bfd4d2a8733b5f36d3d
Tiqur <trevorbrage02@gmail.com>
2023-10-02 09:10:23 -0700

    setup seperate thread for http server

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <pthread.h>
 #include "include/ws_server_events.h"
 #include "include/mongoose.h"
+
+char* message;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+
+    cond = 
+    pthread_cond_signal(&cond);
+
     if (mg_http_match_uri(hm, "/api/hello")) {              // On /api/hello requests,
       mg_http_reply(c, 200, "", "{%m:%d}\n",
                     MG_ESC("status"), 1);                   // Send dynamic JSON response
@@ -15,18 +23,29 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   }
 }
 
+void *start_http_server()
+{
+  struct mg_mgr mgr;                                
+  mg_mgr_init(&mgr);
+  mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);
+  for (;;) mg_mgr_poll(&mgr, 1000);
+}
+
 int main(void)
 {
+  // Start HTTP server on seperate thread
+  printf("Starting HTTP server...\n");
+  pthread_t thread_id;
+  pthread_create(&thread_id, NULL, start_http_server, message);
+
+  // Init websocket 
   struct ws_events evs;
   evs.onopen = &onopen;
   evs.onclose = &onclose;
   evs.onmessage = &onmessage;
 
-  struct mg_mgr mgr;                                
-  mg_mgr_init(&mgr);                                      // Init manager
-  mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
-  for (;;) mg_mgr_poll(&mgr, 1000);                       // Infinite event loop
-
-  //ws_socket(&evs, 3000, 0, 1000);
+  // Start websocket server
+  printf("Starting websocket server...\n");
+  ws_socket(&evs, 3000, 0, 1000);
   return 0;
 }
